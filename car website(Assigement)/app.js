1cc1ee32d3be99dba81b0b3ed3e5c28f0c5fc9fb
Muhammad Ahmed Siddiqui <154745545+mahmedsiddiqui@users.noreply.github.com>
2024-05-21 12:45:14 +0500

    Add files via upload

---
+++ car website(Assigement)/app.js
@@ -0,0 +1,56 @@
+
+function Hilux() {
+    var Hilux = {
+        BestColor: "black",
+        MaxSpeed: "200km/h",
+        price: " PKR 14,419,000.",
+        bodystyle: "SUV'S",
+    }
+    console.log(Hilux);
+    alert("Hilux Description in console")
+}
+
+function Stonic() {
+    var Stonic = {
+        BestColor: "white",
+        MaxSpeed: "220km/h",
+        price: " PKR 4,767,000",
+        bodystyle: "Crossover",
+    }
+    console.log(Stonic);
+    alert("Stonic Description in console")
+}
+function HRV() {
+    var HRV = {
+        BestColor: "Light-Grey",
+        MaxSpeed: "200km/h",
+        price: " PKR 7,899,000",
+        bodystyle: "SUV'S",
+    }
+    console.log(HRV);
+    alert("HRV Description in console" )
+}
+
+
+function Sportage() {
+    var Sportage = {
+        BestColor: "Light-Grey",
+        MaxSpeed: "240km/h",
+        price: " PKR 7,300,000",
+        bodystyle: "SUV'S",
+    }
+    console.log(Sportage);
+    alert("Sportage Description in console")
+}
+
+
+function landcruser() {
+    var Sportage = {
+        BestColor: "Black",
+        MaxSpeed: "240km/h",
+        price: " PKR 8,000,000",
+        bodystyle: "SUV'S",
+    }
+    console.log(landcruser);
+    alert("land cruser Description in console")
+}
